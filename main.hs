ab829cda72c376c0cc12e57c85fe3f4241634b73
Cyoan <yoanrrene2017@gmail.com>
2023-06-13 18:30:02 -0400

    Color bonito

--- main.hs
+++ main.hs
@@ -3,13 +3,16 @@ import Graphics.Gloss.Interface.Pure.Game
 import System.Random
 import Data.List
 import System.IO.Unsafe
+import Graphics.Gloss.Data.Color
+import Graphics.Gloss.Data.Picture
 
 -- Define the game state
 data GameState = GameState {
+  --grid [[Int]],
   snake :: [(Int, Int)],
   direction :: (Int, Int),
   food :: [(Int, Int)],
-  vacio :: [(Int,Int)],
+  obstaculos :: [(Int,Int)],
   gameOver :: Bool,
   score :: Int
 }
@@ -17,17 +20,29 @@ data GameState = GameState {
 -- Define the initial game state
 initialState :: GameState
 initialState = GameState {
-  snake = [(0, 0)] ,
+  obstaculos = randomItems  (0,0) cantObstaculos [], 
+  snake = randomItems (0,0) 1 (obstaculos initialState) ,
+  food = initFood cantHuevos ((snake initialState)++(obstaculos initialState)) ,
   direction = (1, 0),
-  food = initFood cantHuevos [(0,0)],
-  vacio= [],--removeElements (removeElements [(a,b)| a <- [-10..10],b <- [-10..10]] [(0,0)]) (initFood cantHuevos [(0,0)]),
   gameOver = False,
   score=0
 }
 
 
+-- Define la función roundedRectangle para dibujar un rectángulo con bordes suaves
+roundedRectangle :: Float -> Float -> Float -> Picture
+roundedRectangle width height radius = pictures [ 
+  polygon [(radius, 0),
+    (width - radius, 0),
+    (width, radius),
+    (width, height - radius),
+    (width - radius, height),
+    (radius, height),
+    (0, height - radius),
+    (0, radius)]]
+
 initFood :: Int -> [(Int,Int)] -> [(Int,Int)]
-initFood cantHuevos snake = (randomFood (0,0) cantHuevos snake)
+initFood cantHuevos snake = (randomItems (0,0) cantHuevos snake)
 
 removeElements :: Eq a => [a] -> [a] -> [a]
 removeElements xs ys = filter (\x -> not (elem x ys)) xs
@@ -39,33 +54,24 @@ window = InWindow "Yoan-Kevin Snake" (640, 480) (10, 10)
 cantHuevos :: Int
 cantHuevos = 5
 
+cantObstaculos :: Int
+cantObstaculos = 7
+
 verdeOscuro :: Color
 verdeOscuro = makeColor 0.1 0.5 0.1 1.0
-
--- Define the background color
-backgroundColor :: Color
-backgroundColor = makeColorI 0 0 0 255
-
--- Define the snake color
-snakeColor :: Color
-snakeColor = makeColorI 0 255 0 255
-
--- Define the food color
-foodColor :: Color
-foodColor = makeColorI 255 0 0 255
 
 -- Define the size of each cell in the grid
 cellSize :: Float
 cellSize = 20.0
 
 
-randomFood :: (Int,Int) -> Int -> [(Int,Int)] -> [(Int,Int)]
-randomFood _ 0 _ = []
-randomFood (x, y) n invalids = 
+randomItems :: (Int,Int) -> Int -> [(Int,Int)] -> [(Int,Int)]
+randomItems _ 0 _ = []
+randomItems (x, y) n invalids = 
   if (x', y') `elem` invalids 
-    then randomFood (x', y') n invalids 
+    then randomItems (x', y') n invalids 
   else 
-    (x', y'):(randomFood (x,y) (n-1) ((x',y'):invalids))
+    (x', y'):(randomItems (x,y) (n-1) ((x',y'):invalids))
   where
     x1=randomRIO (-10, 10) :: IO Int
     y1=randomRIO (-10, 10) :: IO Int
@@ -74,16 +80,16 @@ randomFood (x, y) n invalids =
 
 updateFood :: [(Int,Int)] -> (Int,Int) -> Int -> [(Int,Int)] -> [(Int,Int)]
 updateFood food (x,y) n invalids=
-  if( length food == 1) then randomFood (x,y) n invalids
+  if( length food == 1) then randomItems (x,y) n invalids
   else delete (x,y) food
 
 -- Define the update function
 update :: Float -> GameState -> GameState
-update _ gameState@(GameState { snake = (x, y):xs, direction = (dx, dy), food = food, gameOver = False ,score=score}) =
-    if x' < -10 || x' > 10 || y' < -10 || y' > 10 || (x', y') `elem` xs
+update _ gameState@(GameState { obstaculos = obstaculos ,snake = (x, y):xs, direction = (dx, dy), food = food, gameOver = False ,score=score}) =
+    if x' < -10 || x' > 10 || y' < -10 || y' > 10 || (x', y') `elem` xs || (x', y') `elem` obstaculos
         then gameState { gameOver = True }
     else if (x', y') `elem` food
-        then gameState { snake = (x', y'):  (x,y):xs, food = updateFood food (x', y') cantHuevos (((x',y'):(x,y):xs)++food) ,score=score+1}
+        then gameState { snake = (x', y'):  (x,y):xs, food = updateFood food (x', y') cantHuevos (((x',y'):(x,y):xs)++food++obstaculos) ,score=score+1}
     else 
         gameState { snake = (x', y') : init ((x,y):xs) }
     where
@@ -108,18 +114,23 @@ handleInput _ gameState = gameState
 
 -- Define the rendering function
 render :: GameState -> Picture
-render GameState { snake = (x,y):xs, food = food, gameOver = False ,score=score, vacio=vacio} =
-  pictures [renderSnake xs , renderFood food , renderScore score ,renderVacias vacio, renderHead (x,y) ]
+render GameState {obstaculos=obstaculos, snake = (x,y):xs, food = food, gameOver = False ,score=score} =
+  pictures [renderCeldas,renderObstaculos obstaculos,renderSnake xs , renderFood food, renderFoodVerde food, renderHead (x,y) ,renderScore score]
   where
     renderHead (x,y) = color verdeOscuro (renderCell (x,y))
-    renderSnake xs = color snakeColor (pictures (map renderCell xs))
-    renderFood food = color foodColor (pictures (map renderCell food))
-    renderVacias vacio = color white (pictures (map renderCell vacio))
-    renderScore score = translate (-300) 200 (scale 0.2 0.2 (color red (text ("Score: "++(show score)))))
-    renderCell (x, y) = translate (fromIntegral x * cellSize) (fromIntegral y * cellSize) (rectangleSolid cellSize cellSize)
+    mapa= [(a,b)| a <- [-10..11],b <- [-10..11]]
+    renderSnake xs = color green (pictures (map renderCell xs))
+    renderFood food = color red (pictures (map renderCell food))
+    renderFoodVerde food = color verdeOscuro (pictures (map renderCellV food))
+    renderObstaculos obstaculos= color blue (pictures (map renderCell obstaculos))
+    renderCeldas = color black (pictures (map renderCellS mapa))
+    renderScore score = translate (-310) 215 (scale 0.15 0.15 (color red (text ("Score: "++(show score)))))
+    renderCellS (x, y) = translate (fromIntegral x * cellSize) (fromIntegral y * cellSize) (rectangleSolid cellSize cellSize)
+    renderCellV (x, y) = translate (fromIntegral x * cellSize) (fromIntegral y * cellSize) (roundedRectangle (cellSize/3) (cellSize/3) 2)
+    renderCell (x, y) = translate (fromIntegral x * cellSize) (fromIntegral y * cellSize) (roundedRectangle cellSize cellSize 4)
 render GameState { gameOver = True } =
   translate (-200) 0 (scale 0.5 0.5 (color red (text "Game Over")))
   
 -- Define the main function
 main :: IO ()
-main = play window backgroundColor 10 initialState render handleInput update
+main = play window white 6 initialState render handleInput update
